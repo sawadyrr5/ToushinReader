9134f532147f1cd4fdd6d87e514f8302e59cf541
riskreturn5@gmail.com <6TBneNy5>
2021-05-21 01:56:44 +0900

    [fix] setup.cfg

--- example/example.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from ToushinReader.core import JPFund
-
-
-isin_code = "JP90C000KAH8"  # グローバルＥＳＧハイクオリティ成長株式ファンド（為替ヘッジなし）
-
-myFund = JPFund(isin_code)
-attrib = myFund.attribute
-print(attrib)
